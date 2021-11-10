06371ee87fdf2d99fb5cdf73601d11a92674c6b1
Sylvie-B <unsolo@hotmail.fr>
2021-11-10 23:14:51 +0100

    color header div

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+let blue = document.getElementById('blue');
+let red = document.getElementById('red');
+let white = document.getElementById('white');
+let black = document.getElementById('black');
+let yellow = document.getElementById('yellow');
+let green = document.getElementById('green');
+
 let container = document.getElementById('frame');
 let frame = container.getElementsByTagName('div');
 console.log(frame);
 console.log(frame.length);
 
+let colorFrame = blue;
+console.log(colorFrame);
+
 container.addEventListener('click', function (){
-    inBlue();
-
-})
-
-
-let inBlue = function (){
-    for (let i = 0 ; i < frame.length; i++){
-        frame[i].addEventListener('mouseover', function (){
-            frame[i].style.backgroundColor = "blue";
-        })
+        for (let i = 0 ; i < frame.length; i++){
+            frame[i].addEventListener('mouseover', function (){
+                frame[i].style.backgroundColor = colorFrame.toString();
+            })
+        }
     }
-}
-
+)
