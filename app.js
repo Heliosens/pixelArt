4ab3cc421589005195fd2a221c54be8769935b0d
Sylvie-B <unsolo@hotmail.fr>
2021-11-10 14:30:25 +0100

    create inBlue function

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-let frame = document.getElementById('frame').getElementsByTagName('div');
+let container = document.getElementById('frame');
+let frame = container.getElementsByTagName('div');
 console.log(frame);
 console.log(frame.length);
 
-for (let i = 0 ; i < frame.length; i++){
-    frame[i].addEventListener('click', function (){
-        frame[i].style.backgroundColor = "blue";
-    })
+container.addEventListener('click', function (){
+    inBlue();
+
+})
+
+
+let inBlue = function (){
+    for (let i = 0 ; i < frame.length; i++){
+        frame[i].addEventListener('mouseover', function (){
+            frame[i].style.backgroundColor = "blue";
+        })
+    }
 }
 
