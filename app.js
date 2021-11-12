409b5ac780da1584f8bcf7b53e75ecdabc4330db
Sylvie-B <unsolo@hotmail.fr>
2021-11-12 02:04:23 +0100

    test function paint & pixel

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
-let blue = document.getElementById('blue');
-let red = document.getElementById('red');
-let white = document.getElementById('white');
-let black = document.getElementById('black');
-let yellow = document.getElementById('yellow');
-let green = document.getElementById('green');
-let orange = document.getElementById('orange');
-
+let main = document.querySelector('main');
 let frame = document.getElementById('frame');
 let frameDiv = frame.getElementsByTagName('div');
-
-console.log(frameDiv.length);
-
+let spanMod = document.getElementById('mod').getElementsByTagName("span");
 let colorBase = document.getElementById('colorBase').getElementsByTagName('div');
+let activColor = document.getElementById('activColor');
+
+main.style.width = innerWidth + "px";
+main.style.height = innerHeight + "px";
 
 let colorFrame = "blue";
 
 for (let i = 0 ; i < colorBase.length ; i++){
     colorBase[i].addEventListener('click', function (){
         colorFrame = colorBase[i].id;
+        activColor.style.backgroundColor = colorFrame;
     })
 }
 
-frame.addEventListener('click', function (){
-        for (let i = 0 ; i < frameDiv.length; i++){
-            frameDiv[i].addEventListener('mouseover', function (){
-                frameDiv[i].style.backgroundColor = colorFrame;
-                frameDiv[i].style.borderColor = colorFrame;
-            })
-        }
+let pixel = function (){
+    for (let i = 0 ; i < frameDiv.length; i++) {
+        frameDiv[i].addEventListener('click', function () {
+            frameDiv[i].style.backgroundColor = colorFrame;
+            // frameDiv[i].style.borderColor = colorFrame;
+        })
     }
-)
+}
+
+let paint = function (){
+    for (let i = 0 ; i < frameDiv.length; i++){
+        frameDiv[i].addEventListener('mouseover', function (){
+            frameDiv[i].style.backgroundColor = colorFrame;
+        })
+    }
+}
+
+for (let i = 0 ; i < spanMod.length ; i++){
+    spanMod[i].addEventListener('click', function (){
+        console.log("i = " + i);
+
+        switch (i){
+            case 0 :
+                console.log("pixel");
+                pixel();
+                break;
+            case 1 :
+                console.log("paint");
+                paint();
+                break;
+        }
+
+    })
+}
 
