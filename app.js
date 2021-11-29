3de39df96078af772a56352e774a6289783fbfd6
Sylvie-B <unsolo@hotmail.fr>
2021-11-29 11:44:10 +0100

    test for size 30

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ let pt = document.getElementById('pt');
 let colorBase = document.getElementById('colorBase').getElementsByTagName('div');
 let activColor = document.getElementById('activColor');
 
-main.style.width = innerWidth + "px";
-main.style.height = innerHeight + "px";
-
 // choice zone size of picture width / height
-let frameWidth = 40;
-let frameHeight = 40;
+let frameWidth = 30;
+let frameHeight = 30;
 
+// create each square
 for(let i = 0 ; i < frameWidth*frameHeight ; i++){
     let square = document.createElement('div');
     let size = innerHeight / frameHeight;
@@ -22,12 +20,22 @@ for(let i = 0 ; i < frameWidth*frameHeight ; i++){
     frame.appendChild(square);
 }
 
+// default color
 let colorFrame = "blue";
+
+/**     todo create colorBase square
+ *      give each colorBase a RGB background color
+ *      on click get the RGB code
+ **/
+
+// create colorBase square
+let numOfColorBase = 12;
+
+
 
 for (let i = 0 ; i < colorBase.length ; i++){
     colorBase[i].addEventListener('click', function (){
-        colorFrame = colorBase[i].style.backgroundColor;
-
+        colorFrame = colorBase[i].id;
         activColor.style.backgroundColor = colorFrame;
     })
 }
